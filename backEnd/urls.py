2fee416b36b39cc329f8d3e4477eed9faa607f85
choki-dorji <62417033+choki-dorji@users.noreply.github.com>
2022-09-08 14:53:59 +0600

    Update urls.py

--- backEnd/urls.py
+++ backEnd/urls.py
@@ -19,4 +19,5 @@ from django.urls import path , include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/',include('api.urls'))
-]
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
